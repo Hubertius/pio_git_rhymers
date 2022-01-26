aed159498d02d1e2077cb5447d16c71a118e462c
Hubertius <hubert.kopec@interia.pl>
2022-01-26 16:45:25 +0100

    1.3.3 Odpowiedź na pytanie w pliku FIFORhymer.java

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -12,5 +12,7 @@ public class FIFORhymer extends defaultCountingOutRhymer {
 		while (!temp.callCheck())
 			countIn(temp.countOut());
 		return ret;
+	// Klawisze (alt + left scroll) || (alt + right scroll) pozwalają 
+	// na przemieszczanie się w tył / przód w dotychczas edytowanych plikach
 	}
 }

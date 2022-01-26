c8d8b1929c6eb544179db6ed128c3f02204f8bb1
Hubertius <hubert.kopec@interia.pl>
2022-01-26 16:43:09 +0100

    1.3.1. 1.3.2 Formatowanie kodu + odpowiedź na pytanie w pliku HanoiRhymer.java

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -2,16 +2,22 @@ package edu.kis.vh.nursery;
 
 public class HanoiRhymer extends defaultCountingOutRhymer {
 
-int totalRejected = 0;
+	int totalRejected = 0;
 
 	public int reportRejected() {
 		return totalRejected;
 	}
 
 	public void countIn(int in) {
-	if (!callCheck() && in > peekaboo())
+		if (!callCheck() && in > peekaboo())
 			totalRejected++;
-			else
-				super.countIn(in);
+		else
+			super.countIn(in);
 	}
+	/*
+	  Brakowało powiększenia wcięcia dla linii 12 oraz 
+	  jego pomniejszenia dla linii 14 (ponadto linie 13 i 15 powinny mieć 
+	  zastosowane jeden tabulator)
+	  
+	*/
 }
